557626e9206c7f61c8a7f39f6b6dded73075d532
Simeon Cholakov <simeonstefanovcholakov@gmail.com>
2023-04-27 10:41:48 +0300

    Created express server

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
