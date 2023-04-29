9216c17e0ebeb4f63c7243de2840ab6629ec8159
Simeon Cholakov <simeonstefanovcholakov@gmail.com>
2023-04-29 19:47:23 +0300

    Corrected isAuthenticated and isOwner bug, added comments and finished the project

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,52 +1,66 @@
 import express from "express";
 import { deleteUserById, getUsers, getUserById } from "../models/users";
 
+// Define function to get all users
 export const getAllUsers = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  try {
+    req: express.Request,
+    res: express.Response
+    ) => {
+    try {
+    // Retrieve all users from database
     const users = await getUsers();
 
+    // Return list of users as JSON response
     return res.status(200).json(users);
   } catch (err) {
+    // Handle errors by logging and returning status code
     console.log(err);
     return res.sendStatus(400);
   }
 };
 
+// Define function to delete a user by ID
 export const deleteUser = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
+    // Retrieve user ID from request parameters
     const { id } = req.params;
 
+    // Delete user with given ID from database
     const deletedUser = await deleteUserById(id);
 
     return res.json(`${deletedUser.username} has been deleted successfully!`);
   } catch (err) {
+    // Handle errors by logging and returning status code
     console.log(err);
     return res.sendStatus(400);
   }
 };
 
+// Define function to update a user
 export const updateUser = async (req: express.Request, res: express.Response) => {
     try {
+        // Retrieve user ID and username from request parameters and body
       const { id } = req.params;
       const { username } = req.body;
   
+      // Return error if username does not exist
       if (!username) {
         return res.sendStatus(400).json(`User with username: ${username} does not exist!`);
       }
   
+      // Retrieve user from database using ID
       const user = await getUserById(id);
       
+      // Update user's username and save changes to database
       user.username = username;
       await user.save();
   
       return res.status(200).json(`Successfully updated ${username}`).end();
     } catch (error) {
+        // Handle errors by logging and returning status code
       console.log(error);
       return res.sendStatus(400);
     }
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,22 +1,30 @@
 import express from 'express';
 import { merge, get } from 'lodash';
+import { getUserBySessionToken } from '../models/users';
+import dotenv from 'dotenv';
 
-import { getUserBySessionToken } from '../models/users'; 
+dotenv.config(); // load environment variables from a .env file
 
+// Middleware to check if the user is authenticated
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
-    const sessionToken = req.cookies['ANTONIO-AUTH'];
+    // Get session token from cookies
+    const sessionToken = req.cookies[process.env.COOKIE_NAME];
 
+    // Check if session token exists
     if (!sessionToken) {
       return res.sendStatus(403);
     }
 
+    // Retrieve user with the given session token
     const existingUser = await getUserBySessionToken(sessionToken);
 
+    // Check if user exists
     if (!existingUser) {
       return res.sendStatus(403);
     }
 
+    // Merge identity of the user with the request object
     merge(req, { identity: existingUser });
 
     return next();
@@ -26,15 +34,20 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
   }
 }
 
+// Middleware to check if the user is the owner of the resource
 export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
+    // Get the id of the user to be checked
     const { id } = req.params;
+    // Get the current user's id
     const currentUserId = get(req, 'identity._id') as string;
 
+    // Check if current user's id exists
     if (!currentUserId) {
       return res.sendStatus(400);
     }
 
+    // Check if current user is the owner of the resource
     if (currentUserId.toString() !== id) {
       return res.sendStatus(403);
     }
